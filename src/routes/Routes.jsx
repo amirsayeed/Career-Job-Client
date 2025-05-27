99e775df796625d5d6d619cf4f984a10a6abf23d
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-05-28 00:59:48 +0600

    firebase project setup,hide credentials and navbar,footer added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,8 @@ import {
 } from "react-router";
 import Root from "../layouts/Root/Root";
 import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import SignIn from "../pages/SignIn/SignIn";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ export const router = createBrowserRouter([
         {
             index: true,
             Component: Home
+        },
+        {
+            path: 'register',
+            Component: Register
+        },
+        {
+            path: 'signIn',
+            Component: SignIn
         }
     ]
   },
