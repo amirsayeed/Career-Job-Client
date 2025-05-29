e30ef338374248091fc7320470cf6df9e3d8c91d
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-05-29 12:50:10 +0600

    save application using axios js

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, useParams } from 'react-router';
 import useAuth from '../../Hooks/useAuth';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const JobApply = () => {
     const {id:jobId} = useParams();
@@ -13,7 +15,32 @@ const JobApply = () => {
         const linkedIn = form.linkedIn.value;
         const github = form.github.value;
         const resume = form.resume.value;
-        console.log(linkedIn,github,resume)
+        //console.log(linkedIn,github,resume)
+
+        const application = {
+            jobId,
+            applicant: user.email,
+            linkedIn,
+            github,
+            resume
+        }
+
+        axios.post('http://localhost:5000/applications', application)
+        .then(res=>{
+            console.log(res.data); 
+            if(res.data.insertedId){
+                Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your application has been submitted",
+                showConfirmButton: false,
+                timer: 1500
+                });
+            }
+        })
+        .catch(error=>{
+            console.log(error);
+        })
     }
 
     return (
