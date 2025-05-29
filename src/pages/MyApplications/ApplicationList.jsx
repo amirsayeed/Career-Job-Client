0e98d3654e5fba18850264cf5904609db2f755a5
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-05-29 14:38:55 +0600

    applied jobs showed in table format

--- src/pages/MyApplications/ApplicationList.jsx
+++ src/pages/MyApplications/ApplicationList.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { use } from 'react';
+import JobApplicationRow from './JobApplicationRow';
 
-const ApplicationList = () => {
+const ApplicationList = ({myApplicationsPromise}) => {
+    const myApplications = use(myApplicationsPromise);
+    console.log(myApplications);
     return (
-        <div>
-            
+        <div className='my-10'>
+            <h3 className="text-4xl font-bold">Jobs Applied So Far: {myApplications.length}</h3>
+
+            <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                     <tr>
+                    <th>
+                        #
+                    </th>
+                    <th>Name</th>
+                    <th>Job</th>
+                    <th>Favorite Color</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                    myApplications.map((application,index)=> <JobApplicationRow key={application._id} index={index} application={application}/>)
+                    }
+                </tbody>
+            </table>
+            </div>
         </div>
     );
 };
--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -2,13 +2,16 @@ import React, { Suspense } from 'react';
 import ApplicationStats from './ApplicationStats';
 import ApplicationList from './ApplicationList';
 import Loading from '../Shared/Loading';
+import { myApplicationsPromise } from '../../api/applicationsApi';
+import useAuth from '../../Hooks/useAuth';
 
 const MyApplications = () => {
+    const {user} = useAuth();
     return (
         <div>
             <ApplicationStats/>
             <Suspense fallback={<Loading/>}>
-                <ApplicationList/>
+                <ApplicationList myApplicationsPromise = {myApplicationsPromise(user.email)}/>
             </Suspense>
         </div>
     );
