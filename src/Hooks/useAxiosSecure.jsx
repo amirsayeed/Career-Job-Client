3d932643e17e5b63532754375bc640ca79f2297d
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-06-02 15:19:29 +0600

    handled 401 and 403 in response interceptor

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -7,12 +7,28 @@ const axiosInstance = axios.create({
 })
 
 const useAxiosSecure = () => {
-    const {user} = useAuth();
-    
+    const {user,signOutUser} = useAuth();
+
     axiosInstance.interceptors.request.use(config=>{
        config.headers.authorization = `Bearer ${user.accessToken}`
        return config; 
     })
+
+    axiosInstance.interceptors.response.use(response=>{
+        return response;
+    }, error=>{
+        if(error.status === 401 || error.status === 403){
+            signOutUser()
+            .then(()=>{
+                console.log('sign out user for 401 status code')
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+        }
+        return Promise.reject(error)
+    })
+
     return axiosInstance;
 };
 
